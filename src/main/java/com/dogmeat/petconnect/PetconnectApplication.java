9496c24d985e509a4c0e2fdc79ee619aa7a5650f
조무현 <jomuhyeon@jomuhyeons-MacBook-Air.local>
2024-09-25 11:25:45 +0900

    Initial commit

---
+++ src/main/java/com/dogmeat/petconnect/PetconnectApplication.java
@@ -0,0 +1,13 @@
+package com.dogmeat.petconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogmeat/petconnect/PetconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogmeat.petconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

755aa75fce46fdf84ec3d2538673fa3fcf4ab1c7
1TAEKIM <oneteak1@gmail.com>
2024-09-26 15:55:41 +0900

    Add UserController for handling user registration and login

---
+++ src/main/java/com/dogmeat/petconnect/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.dogmeat.petconnect.controller;
+
+import com.dogmeat.petconnect.model.User;
+import com.dogmeat.petconnect.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
